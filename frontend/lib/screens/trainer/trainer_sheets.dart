bc13cba454428f4c73c9fd3956b9f7634acffb0a
Anderson Chaves <andersonchaves30@gmail.com>
2026-05-08 02:23:11 -0300

    fix(imports): remover imports não utilizados
    - splash_screen: remover app_colors
    - notifications_screen: remover app_routes
    - chat_screen: remover go_router e app_routes
    - nutrition_screen: remover go_router e animate_do
    - trainer_sheets: remover go_router
    - metrics_screen: remover progression_line_chart
    - notification_service: remover dart:convert

--- frontend/lib/screens/trainer/trainer_sheets.dart
+++ frontend/lib/screens/trainer/trainer_sheets.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:provider/provider.dart';
 import '../../theme/app_colors.dart';
